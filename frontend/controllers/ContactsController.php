a2619ccb9d89eb667681bed32da67358ec16a22d
Senkevich Kirill <sks89@mail.ru>
2019-09-24 15:49:17 +0300

    Добавляет в контроллер ContactsController рендеринг шаблона страницы

--- frontend/controllers/ContactsController.php
+++ frontend/controllers/ContactsController.php
@@ -6,21 +6,6 @@ class ContactsController extends Controller
 {
     public function actionIndex()
     {
-
-    }
-
-    public function actionView($id)
-    {
-
-    }
-
-    public function actionCreate()
-    {
-
-    }
-
-    public function actionDelete($id)
-    {
-
+        return $this->render('index');
     }
 }
---
+++ frontend/views/contacts/index.php
@@ -0,0 +1,4 @@
+<?php
+?>
+
+<h1>hello world</h1>
