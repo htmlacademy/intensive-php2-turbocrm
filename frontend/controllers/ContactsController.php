c360426c69a9c871a9fb5d168a3f5ea0caad5802
Senkevich Kirill <sks89@mail.ru>
2019-09-26 23:06:17 +0300

    Пример построения запроса на основе выбранных значений в фильтре

--- frontend/controllers/ContactsController.php
+++ frontend/controllers/ContactsController.php
@@ -2,6 +2,7 @@
 namespace frontend\controllers;
 use frontend\models\Company;
 use frontend\models\Contact;
+use Yii;
 use yii\db\Query;
 use yii\web\Controller;
 
@@ -10,9 +11,17 @@ class ContactsController extends Controller
     public function actionIndex()
     {
         $query = new Query();
-        $query->select(['c.name', 'c.phone', 'company.name as company'])->from('contact c')
+        $query->select(['c.name', 'c.phone', 'company.name as company', 'position', 'c.email'])->from('contact c')
             ->join('INNER JOIN', 'company', 'c.company_id = company.id')
             ->orderBy(['c.dt_add' => SORT_ASC]);
+
+        // добавим фильтрацию, если был нужный параметр запроса
+        if ($status = Yii::$app->request->get('status')) {
+            $query->andWhere(['status' => $status]);
+        }
+
+        // или можно так
+        $query->andFilterWhere(['company_id' => Yii::$app->request->get('company')]);
 
         // выполнить запрос и получить результат в виде двухмерного массива
         $rows = $query->all();
