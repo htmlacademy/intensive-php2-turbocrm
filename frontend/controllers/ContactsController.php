745e6a4b1a278803e89a92086b3b0035d9d87cb2
Senkevich Kirill <sks89@mail.ru>
2019-09-26 22:01:11 +0300

    Простой пример использования построителя запросов

--- frontend/controllers/ContactsController.php
+++ frontend/controllers/ContactsController.php
@@ -2,21 +2,21 @@
 namespace frontend\controllers;
 use frontend\models\Company;
 use frontend\models\Contact;
+use yii\db\Query;
 use yii\web\Controller;
 
 class ContactsController extends Controller
 {
     public function actionIndex()
     {
-        $contact = Contact::findOne(['email' => 'den4ik@mail.ru']);
+        $query = new Query();
+        $query->select(['id', 'name', 'phone'])->from('contact')->where(['position' => 'Менеджер'])->limit(10);
 
-//         показывает название компании, к которой принадлежит контакт
-        print($contact->company->name);
+        // выполнить запрос и получить результат в виде двухмерного массива
+        $rows = $query->all();
 
-        $company = Company::findOne(1);
-        $contacts = $company->contacts;
-
-        // массив с объектами-клиентами этой компании
-        var_dump($contacts);
+        foreach ($rows as $row) {
+            print($row['name']);
+        }
     }
 }
