0838c6baee9c27fc5a15a5ec87e9dc382754defe
Senkevich Kirill <sks89@mail.ru>
2019-09-27 21:48:06 +0300

    Добавляет поддержку ЧПУ

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,11 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
