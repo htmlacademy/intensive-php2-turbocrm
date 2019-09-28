39518ee330bacd27525eeb9297c057648a7feb17
Senkevich Kirill <sks89@mail.ru>
2019-09-28 21:18:36 +0300

    Добавляет ActiveForm на страницу регистрации

--- frontend/views/signup/index.php
+++ frontend/views/signup/index.php
@@ -1,3 +1,12 @@
-<div class="registration">
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm; ?>
 
+<div class="registration">
+   <?php $form = ActiveForm::begin([
+      'id' => 'signup-form',
+       'options' => ['class' => 'registration__form']
+]); ?>
+   <p class="registration__title header-1">Регистрация</p>
+<?php ActiveForm::end(); ?>
 </div>
