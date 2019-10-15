fbea18b4e496bf41fa82e7ed67125514ab4cc4b3
Kirill Senkevich <frexin@htmlacademy.ru>
2019-10-15 14:51:59 +0300

    Добавляет пример получения информации о залогиненном пользователе

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -1,12 +1,23 @@
 <?php
 namespace frontend\controllers;
+use frontend\models\User;
 use yii\web\Controller;
 
 class UserController extends Controller
 {
-    public function actionLogout() {
+    public function actionLogout()
+    {
         \Yii::$app->user->logout();
 
         return $this->goHome();
+    }
+
+    public function actionProfile()
+    {
+        if ($id = \Yii::$app->user->getId()) {
+            $user = User::findOne($id);
+
+            print($user->email);
+        }
     }
 }
