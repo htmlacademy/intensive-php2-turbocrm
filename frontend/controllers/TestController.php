61c696242921ffb955ad02541f9e91ff71d324b8
Senkevich Kirill <sks89@mail.ru>
2019-11-14 23:31:44 +0300

    Добавляет отправку API запроса через guzzle

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -2,14 +2,23 @@
 
 namespace frontend\controllers;
 
+use Yii;
 use yii\web\Controller;
 use GuzzleHttp\Client;
 
 class TestController extends Controller
 {
-
     public function actionIndex()
     {
+        $email = Yii::$app->request->get('email', 'john@smith.com');
+        $api_key = Yii::$app->params['apiKey'];
 
+        $client = new Client([
+            'base_uri' => 'https://apilayer.net/api/',
+        ]);
+
+        $response = $client->request('GET', 'check', [
+            'query' => ['email' => $email, 'access_key' => $api_key]
+        ]);
     }
 }
