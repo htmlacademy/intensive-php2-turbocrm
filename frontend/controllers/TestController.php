4cf687f0e2ed3f63afcbe2334b78efdc7b5b2ddb
Senkevich Kirill <sks89@mail.ru>
2019-11-14 22:49:34 +0300

    Устанавливает библиотеку guzzle и добавляет тестовый контроллер для демонстрации использования

---
+++ frontend/controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+use GuzzleHttp\Client;
+
+class TestController extends Controller
+{
+
+    public function actionIndex()
+    {
+
+    }
+}
