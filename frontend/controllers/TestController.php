41cf66de8854514b7cee3a21337474b00d5cdff1
Senkevich Kirill <sks89@mail.ru>
2019-11-16 00:03:09 +0300

    Получает ответ от API и выносит вердикт о существовании проверяемого email

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -20,5 +20,16 @@ class TestController extends Controller
         $response = $client->request('GET', 'check', [
             'query' => ['email' => $email, 'access_key' => $api_key]
         ]);
+
+        $content = $response->getBody()->getContents();
+        $response_data = json_decode($content, true);
+
+        $result = false;
+
+        if (is_array($response_data)) {
+            $result = !empty($response_data['mx_found']) && !empty($response_data['smtp_check']);
+        }
+
+        var_dump("Результат проверки $email", $result);
     }
 }
