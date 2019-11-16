163110550fb4e039193d51a0d259022a5bb12ca5
Senkevich Kirill <sks89@mail.ru>
2019-11-16 18:32:58 +0300

    Добавляет HTTP-клиенту обработку ошибок на код ответа и формат

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -2,8 +2,12 @@
 
 namespace frontend\controllers;
 
+use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Psr7\Request;
 use Yii;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 use GuzzleHttp\Client;
 
@@ -19,12 +23,25 @@ class TestController extends Controller
         ]);
 
         try {
-            $response = $client->request('GET', 'check', [
+            $request = new Request('GET', 'check');
+            $response = $client->send($request, [
                 'query' => ['email' => $email, 'access_key' => $api_key]
             ]);
 
+            if ($response->getStatusCode() !== 200) {
+                throw new BadResponseException("Response error: " . $response->getReasonPhrase(), $request);
+            }
+
             $content = $response->getBody()->getContents();
             $response_data = json_decode($content, true);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw new ServerException("Invalid json format", $request);
+            }
+
+            if ($error = ArrayHelper::getValue($response_data, 'error.info')) {
+                throw new BadResponseException("API error: " . $error, $request);
+            }
 
             $result = false;
 
