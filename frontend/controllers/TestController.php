5295878943c0e49c079a7fb96efd4a80adf77a41
Senkevich Kirill <sks89@mail.ru>
2019-11-16 19:08:59 +0300

    Делает автономный валидатор для проверки существования email

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -2,6 +2,7 @@
 
 namespace frontend\controllers;
 
+use frontend\validators\RemoteEmailValidator;
 use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\ServerException;
@@ -16,41 +17,9 @@ class TestController extends Controller
     public function actionIndex()
     {
         $email = Yii::$app->request->get('email', 'john@smith.com');
-        $api_key = Yii::$app->params['apiKey'];
 
-        $client = new Client([
-            'base_uri' => 'https://apilayer.net/api/',
-        ]);
-
-        try {
-            $request = new Request('GET', 'check');
-            $response = $client->send($request, [
-                'query' => ['email' => $email, 'access_key' => $api_key]
-            ]);
-
-            if ($response->getStatusCode() !== 200) {
-                throw new BadResponseException("Response error: " . $response->getReasonPhrase(), $request);
-            }
-
-            $content = $response->getBody()->getContents();
-            $response_data = json_decode($content, true);
-
-            if (json_last_error() !== JSON_ERROR_NONE) {
-                throw new ServerException("Invalid json format", $request);
-            }
-
-            if ($error = ArrayHelper::getValue($response_data, 'error.info')) {
-                throw new BadResponseException("API error: " . $error, $request);
-            }
-
-            $result = false;
-
-            if (is_array($response_data)) {
-                $result = !empty($response_data['mx_found']) && !empty($response_data['smtp_check']);
-            }
-        } catch (RequestException $e) {
-            $result = true;
-        }
+        $validator = new RemoteEmailValidator();
+        $result = $validator->validate($email);
 
         var_dump("Результат проверки $email", $result);
     }
