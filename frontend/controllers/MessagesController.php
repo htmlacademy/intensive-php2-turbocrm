9f2e0c2d06ac781dff96880e0d891da17b632723
Senkevich Kirill <sks89@mail.ru>
2019-12-03 14:49:29 +0300

    Добавляет пример формы входа в родной стилизации

--- frontend/controllers/MessagesController.php
+++ frontend/controllers/MessagesController.php
@@ -9,7 +9,7 @@ class MessagesController extends Controller
 {
 
     public function actionMail() {
-
+        Yii::$app->cache->set($data, 'gmail_inbox_margo', 86400);
     }
 
 }
--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -1,10 +1,13 @@
 <?php
 namespace frontend\controllers;
+use frontend\models\LoginForm;
 use frontend\models\User;
 use yii\web\Controller;
 
 class UserController extends Controller
 {
+    public $layout = 'anon';
+
     public function actionLogout()
     {
         \Yii::$app->user->logout();
@@ -19,5 +22,24 @@ class UserController extends Controller
 
             print($user->email);
         }
+    }
+
+    public function actionLogin()
+    {
+        $loginForm = new LoginForm();
+
+        if (\Yii::$app->request->getIsPost()) {
+            $loginForm->load(\Yii::$app->request->post());
+
+            if ($loginForm->validate()) {
+                $user = $loginForm->getUser();
+
+                \Yii::$app->user->login($user);
+
+                return $this->goHome();
+            }
+        }
+
+        return $this->render('login', ['model' => $loginForm]);
     }
 }
